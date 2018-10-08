d41b70cfebc279bc5801bd116d0ab9191b37ed91
Tom Carey <tom@cactuslab.com>
2018-10-08 20:48:49 +1300

    fix null pointer exception

--- app/src/main/java/com/erpdevelopment/vbvm/MediaStyleHelper.java
+++ app/src/main/java/com/erpdevelopment/vbvm/MediaStyleHelper.java
@@ -26,15 +26,19 @@ public class MediaStyleHelper {
             Context context, MediaSessionCompat mediaSession) {
         MediaControllerCompat controller = mediaSession.getController();
         MediaMetadataCompat mediaMetadata = controller.getMetadata();
-        MediaDescriptionCompat description = mediaMetadata.getDescription();
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-        builder
-                .setContentTitle(description.getTitle())
-                .setContentText(description.getSubtitle())
-                .setSubText(description.getDescription())
-                .setLargeIcon(description.getIconBitmap())
-                .setContentIntent(controller.getSessionActivity())
+
+        if (mediaMetadata != null) {
+            MediaDescriptionCompat description = mediaMetadata.getDescription();
+            builder
+                    .setContentTitle(description.getTitle())
+                    .setContentText(description.getSubtitle())
+                    .setSubText(description.getDescription())
+                    .setLargeIcon(description.getIconBitmap());
+        }
+
+        builder.setContentIntent(controller.getSessionActivity())
                 .setDeleteIntent(
                         MediaButtonReceiver.buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_STOP))
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
